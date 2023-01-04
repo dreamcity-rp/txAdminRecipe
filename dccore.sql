5a7a527d671effdbaa13f74e157e3c9fabab2309
bsk193 <98500231+bsk420@users.noreply.github.com>
2023-01-04 06:23:10 +0000

    Update

--- dccore.sql
+++ dccore.sql
@@ -350,6 +350,61 @@ CREATE TABLE IF NOT EXISTS `ox_doorlock` (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=8;
 
+CREATE TABLE IF NOT EXISTS `mdw_reports` (
+  `id` varchar(100) NOT NULL,
+  `incident` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `ongoing` tinyint(1) NOT NULL,
+  `expire` varchar(100) NOT NULL,
+  `created` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mdw_incidents` (
+  `id` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `data` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mdw_evidence` (
+  `id` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mdw_announcements` (
+  `id` int(11) NOT NULL,
+  `announcement` longtext NOT NULL,
+  `date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `vehicle_parts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(100) NOT NULL,
+  `parts` longtext NOT NULL,
+  `mileage` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `mdw_announcements`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `mdw_announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `players`
+ADD (`mdw_image` varchar(1000) COLLATE utf8mb4_bin NOT NULL,
+  `mdw_description` longtext COLLATE utf8mb4_bin NOT NULL,
+  `mdw_alias` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `mdw_staffdata` longtext COLLATE utf8mb4_bin NOT NULL);
+  
+ALTER TABLE `players` CHANGE `mdw_description` `mdw_description` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL, CHANGE `mdw_staffdata` `mdw_staffdata` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL;
+
+ALTER TABLE `player_vehicles` ADD `impounded` BOOLEAN NOT NULL, ADD `mdw_image` VARCHAR(200) NOT NULL;
+
 INSERT INTO `ox_doorlock` (`id`, `name`, `data`) VALUES
 	(1, 'character_room', '{"maxDistance":2,"coords":{"x":-809.2808837890625,"y":177.85536193847657,"z":76.89032745361328},"heading":201,"doors":false,"passcode":"5555","model":-384976104,"state":1}'),
 	(2, 'character_house', '{"maxDistance":2,"coords":{"x":-816.411376953125,"y":178.30441284179688,"z":72.82737731933594},"state":1,"doors":[{"coords":{"x":-816.1068115234375,"y":177.5108642578125,"z":72.82737731933594},"heading":291,"model":-1686014385},{"coords":{"x":-816.7160034179688,"y":179.09796142578126,"z":72.82737731933594},"heading":291,"model":159994461}],"passcode":"5555"}'),
