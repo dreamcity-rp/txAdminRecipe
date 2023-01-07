2bb56e3a545ec8d74e9c4d70635ba65f375f7ae1
bsk193 <98500231+bsk420@users.noreply.github.com>
2023-01-07 03:18:47 +0000

    Update

--- dccore.sql
+++ dccore.sql
@@ -120,71 +120,6 @@ CREATE TABLE IF NOT EXISTS `occasion_vehicles` (
   KEY `occasionId` (`occasionid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
-CREATE TABLE IF NOT EXISTS `phone_invoices` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `amount` int(11) NOT NULL DEFAULT 0,
-  `society` tinytext DEFAULT NULL,
-  `sender` varchar(50) DEFAULT NULL,
-  `sendercitizenid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `phone_gallery` (
-   `citizenid` VARCHAR(255) NOT NULL ,
-   `image` VARCHAR(255) NOT NULL ,
-   `date` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `player_mails` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `sender` varchar(50) DEFAULT NULL,
-  `subject` varchar(50) DEFAULT NULL,
-  `message` text DEFAULT NULL,
-  `read` tinyint(4) DEFAULT 0,
-  `mailid` int(11) DEFAULT NULL,
-  `date` timestamp NULL DEFAULT current_timestamp(),
-  `button` text DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `phone_messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `number` varchar(50) DEFAULT NULL,
-  `messages` text DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`),
-  KEY `number` (`number`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `phone_tweets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `firstName` varchar(25) DEFAULT NULL,
-  `lastName` varchar(25) DEFAULT NULL,
-  `message` text DEFAULT NULL,
-  `date` datetime DEFAULT current_timestamp(),
-  `url` text DEFAULT NULL,
-  `picture` varchar(512) DEFAULT './img/default.png',
-  `tweetId` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `player_contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `number` varchar(50) DEFAULT NULL,
-  `iban` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `citizenid` (`citizenid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
-
 CREATE TABLE IF NOT EXISTS `players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) NOT NULL,
@@ -263,51 +198,6 @@ CREATE TABLE IF NOT EXISTS `player_warns` (
   `warnId` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
-
-CREATE TABLE IF NOT EXISTS `bank_accounts` (
-  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(250) DEFAULT NULL,
-  `business` varchar(50) DEFAULT NULL,
-  `businessid` int(11) DEFAULT NULL,
-  `gangid` varchar(50) DEFAULT NULL,
-  `amount` bigint(255) NOT NULL DEFAULT 0,
-  `account_type` enum('Current','Savings','business','Gang') NOT NULL DEFAULT 'Current',
-  PRIMARY KEY (`record_id`),
-  UNIQUE KEY `citizenid` (`citizenid`),
-  KEY `business` (`business`),
-  KEY `businessid` (`businessid`),
-  KEY `gangid` (`gangid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `bank_statements` (
-  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) DEFAULT NULL,
-  `account` varchar(50) DEFAULT NULL,
-  `business` varchar(50) DEFAULT NULL,
-  `businessid` int(11) DEFAULT NULL,
-  `gangid` varchar(50) DEFAULT NULL,
-  `deposited` int(11) DEFAULT NULL,
-  `withdraw` int(11) DEFAULT NULL,
-  `balance` int(11) DEFAULT NULL,
-  `date` varchar(50) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`record_id`),
-  KEY `business` (`business`),
-  KEY `businessid` (`businessid`),
-  KEY `gangid` (`gangid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `bank_cards` (
-  `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50),
-  `cardNumber` varchar(50) DEFAULT NULL,
-  `cardPin` varchar(50) DEFAULT NULL,
-  `cardActive` tinyint(4) DEFAULT 1,
-  `cardLocked` tinyint(4) DEFAULT 0,
-  `cardType` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`citizenid`),
-  KEY `record_id` (`record_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `gloveboxitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -388,6 +278,334 @@ CREATE TABLE IF NOT EXISTS `vehicle_parts` (
   `mileage` float NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `pefcl_accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `accountName` varchar(255) DEFAULT NULL,
+  `isDefault` tinyint(1) DEFAULT '0',
+  `ownerIdentifier` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT 'owner',
+  `balance` int DEFAULT '25000',
+  `type` varchar(255) DEFAULT 'personal',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `number` (`number`),
+  UNIQUE KEY `number_2` (`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- dev.pefcl_cash definition
+
+CREATE TABLE IF NOT EXISTS `pefcl_cash` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `amount` int DEFAULT '2000',
+  `ownerIdentifier` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ownerIdentifier` (`ownerIdentifier`),
+  UNIQUE KEY `ownerIdentifier_2` (`ownerIdentifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- dev.pefcl_external_accounts definition
+
+CREATE TABLE IF NOT EXISTS `pefcl_external_accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pefcl_external_accounts_user_id_number` (`userId`,`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- dev.pefcl_invoices definition
+
+CREATE TABLE IF NOT EXISTS `pefcl_invoices` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) NOT NULL,
+  `from` varchar(255) NOT NULL,
+  `to` varchar(255) NOT NULL,
+  `fromIdentifier` varchar(255) NOT NULL,
+  `toIdentifier` varchar(255) NOT NULL,
+  `receiverAccountIdentifier` varchar(255) DEFAULT NULL,
+  `amount` int DEFAULT '0',
+  `status` varchar(255) DEFAULT 'PENDING',
+  `expiresAt` datetime NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- dev.pefcl_shared_accounts definition
+
+CREATE TABLE IF NOT EXISTS `pefcl_shared_accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userIdentifier` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT 'contributor',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  `accountId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `accountId` (`accountId`),
+  CONSTRAINT `pefcl_shared_accounts_ibfk_1` FOREIGN KEY (`accountId`) REFERENCES `pefcl_accounts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- dev.pefcl_transactions definition
+
+CREATE TABLE IF NOT EXISTS `pefcl_transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) DEFAULT NULL,
+  `amount` int DEFAULT '0',
+  `type` varchar(255) DEFAULT 'Outgoing',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `toAccountId` int DEFAULT NULL,
+  `fromAccountId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `toAccountId` (`toAccountId`),
+  KEY `fromAccountId` (`fromAccountId`),
+  CONSTRAINT `pefcl_transactions_ibfk_1` FOREIGN KEY (`toAccountId`) REFERENCES `pefcl_accounts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `pefcl_transactions_ibfk_2` FOREIGN KEY (`fromAccountId`) REFERENCES `pefcl_accounts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `npwd_twitter_profiles`
+(
+    `id`           int         NOT NULL AUTO_INCREMENT,
+    `profile_name` varchar(90) NOT NULL,
+    `identifier`   varchar(48) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `avatar_url`   varchar(255)         DEFAULT 'https://i.file.glass/QrEvq.png',
+    `createdAt`    timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`    timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `profile_name_UNIQUE` (`profile_name`),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_phone_contacts`
+(
+    `id`         int(11)      NOT NULL AUTO_INCREMENT,
+    `identifier` varchar(48)           DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `avatar`     varchar(255)          DEFAULT NULL,
+    `number`     varchar(20)           DEFAULT NULL,
+    `display`    varchar(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_twitter_tweets`
+(
+    `id`         INT(11)       NOT NULL AUTO_INCREMENT,
+    `message`    VARCHAR(1000) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `createdAt`  TIMESTAMP     NOT NULL DEFAULT current_timestamp(),
+    `updatedAt`  TIMESTAMP     NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `likes`      INT(11)       NOT NULL DEFAULT '0',
+    `identifier` VARCHAR(48)   NOT NULL COLLATE 'utf8mb4_general_ci',
+    `visible`    TINYINT(4)    NOT NULL DEFAULT '1',
+    `images`     VARCHAR(1000) NULL     DEFAULT '' COLLATE 'utf8mb4_general_ci',
+    `retweet`    INT(11)       NULL     DEFAULT NULL,
+    `profile_id` INT(11)       NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `npwd_twitter_tweets_npwd_twitter_profiles_id_fk` (`profile_id`) USING BTREE,
+    CONSTRAINT `npwd_twitter_tweets_npwd_twitter_profiles_id_fk` FOREIGN KEY (`profile_id`) REFERENCES `npwd_twitter_profiles` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_twitter_likes`
+(
+    `id`         int NOT NULL AUTO_INCREMENT,
+    `profile_id` int NOT NULL,
+    `tweet_id`   int NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_combination` (`profile_id`, `tweet_id`),
+    KEY `profile_idx` (`profile_id`),
+    KEY `tweet_idx` (`tweet_id`),
+    CONSTRAINT `profile` FOREIGN KEY (`profile_id`) REFERENCES `npwd_twitter_profiles` (`id`),
+    CONSTRAINT `tweet` FOREIGN KEY (`tweet_id`) REFERENCES `npwd_twitter_tweets` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_match_profiles`
+(
+    `id`            int          NOT NULL AUTO_INCREMENT,
+    `identifier`    varchar(48)  NOT NULL COLLATE 'utf8mb4_general_ci',
+    `name`          varchar(90)  NOT NULL,
+    `image`         varchar(255) NOT NULL,
+    `bio`           varchar(512)          DEFAULT NULL,
+    `location`      varchar(45)           DEFAULT NULL,
+    `job`           varchar(45)           DEFAULT NULL,
+    `tags`          varchar(255) NOT NULL DEFAULT '',
+    `voiceMessage`  varchar(512)         DEFAULT NULL,
+    `createdAt`     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `identifier_UNIQUE` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_match_views`
+(
+    `id`         int         NOT NULL AUTO_INCREMENT,
+    `identifier` varchar(48) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `profile`    int         NOT NULL,
+    `liked`      tinyint              DEFAULT '0',
+    `createdAt`  timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`  timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `match_profile_idx` (`profile`),
+    CONSTRAINT `match_profile` FOREIGN KEY (`profile`) REFERENCES `npwd_match_profiles` (`id`),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_notes`
+(
+    `id`         int(11)      NOT NULL AUTO_INCREMENT,
+    `identifier` varchar(48)  NOT NULL COLLATE 'utf8mb4_general_ci',
+    `title`      varchar(255) NOT NULL,
+    `content`    varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_marketplace_listings`
+(
+    `id`          int(11)      NOT NULL AUTO_INCREMENT,
+    `identifier`  varchar(48)           DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `username`    varchar(255)          DEFAULT NULL,
+    `name`        varchar(50)           DEFAULT NULL,
+    `number`      varchar(255) NOT NULL,
+    `title`       varchar(255)          DEFAULT NULL,
+    `url`         varchar(255)          DEFAULT NULL,
+    `description` varchar(255) NOT NULL,
+    `createdAt`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `reported`    tinyint      NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_twitter_reports`
+(
+    `id`         int NOT NULL AUTO_INCREMENT,
+    `profile_id` int NOT NULL,
+    `tweet_id`   int NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_combination` (`profile_id`, `tweet_id`),
+    KEY `profile_idx` (`profile_id`),
+    KEY `tweet_idx` (`tweet_id`),
+    CONSTRAINT `report_profile` FOREIGN KEY (`profile_id`) REFERENCES `npwd_twitter_profiles` (`id`),
+    CONSTRAINT `report_tweet` FOREIGN KEY (`tweet_id`) REFERENCES `npwd_twitter_tweets` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_messages`
+(
+    `id`              int(11)      NOT NULL AUTO_INCREMENT,
+    `message`         varchar(512) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `user_identifier` varchar(48)  NOT NULL COLLATE 'utf8mb4_general_ci',
+    `conversation_id` varchar(512) NOT NULL,
+    `isRead`          tinyint(4)   NOT NULL DEFAULT 0,
+    `createdAt`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `updatedAt`       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP,
+    `visible`         tinyint(4)   NOT NULL DEFAULT 1,
+    `author`          varchar(255) NOT NULL,
+    `is_embed`        tinyint(4)   NOT NULL default 0,
+    `embed`           varchar(512) NOT NULL DEFAULT '',
+    PRIMARY KEY (id),
+    INDEX `user_identifier` (`user_identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_messages_conversations`
+(
+    `id`                INT(11)      NOT NULL AUTO_INCREMENT,
+    `conversation_list` VARCHAR(225) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `label`             VARCHAR(60)  NULL     DEFAULT '' COLLATE 'utf8mb4_general_ci',
+    `createdAt`         TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
+    `updatedAt`         TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
+    `last_message_id`   INT(11)      NULL     DEFAULT NULL,
+    `is_group_chat`     TINYINT(4)   NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`) USING BTREE
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_messages_participants`
+(
+    `id`              INT(11)      NOT NULL AUTO_INCREMENT,
+    `conversation_id` INT(11)      NOT NULL,
+    `participant`     VARCHAR(225) NOT NULL COLLATE 'utf8mb4_general_ci',
+    `unread_count`    INT(11)      NULL DEFAULT '0',
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `message_participants_npwd_messages_conversations_id_fk` (`conversation_id`) USING BTREE,
+    CONSTRAINT `message_participants_npwd_messages_conversations_id_fk` FOREIGN KEY (`conversation_id`) REFERENCES `npwd_messages_conversations` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_calls`
+(
+    `id`          int(11)      NOT NULL AUTO_INCREMENT,
+    `identifier`  varchar(48)  DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `transmitter` varchar(255) NOT NULL,
+    `receiver`    varchar(255) NOT NULL,
+    `is_accepted` tinyint(4)   DEFAULT 0,
+    `start`       varchar(255) DEFAULT NULL,
+    end           varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_phone_gallery`
+(
+    `id`         int(11)      NOT NULL AUTO_INCREMENT,
+    `identifier` varchar(48) DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `image`      varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX `identifier` (`identifier`)
+);
+
+CREATE TABLE IF NOT EXISTS `npwd_darkchat_channels` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`channel_identifier` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`label` VARCHAR(255) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `darkchat_channels_channel_identifier_uindex` (`channel_identifier`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=20
+;
+
+CREATE TABLE IF NOT EXISTS `npwd_darkchat_channel_members` (
+	`channel_id` INT(11) NOT NULL,
+	`user_identifier` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`is_owner` TINYINT(4) NOT NULL DEFAULT '0',
+	INDEX `npwd_darkchat_channel_members_npwd_darkchat_channels_id_fk` (`channel_id`) USING BTREE,
+	CONSTRAINT `npwd_darkchat_channel_members_npwd_darkchat_channels_id_fk` FOREIGN KEY (`channel_id`) REFERENCES `npwd_darkchat_channels` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE IF NOT EXISTS `npwd_darkchat_messages` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`channel_id` INT(11) NOT NULL,
+	`message` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`user_identifier` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`createdAt` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	`is_image` TINYINT(4) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`) USING BTREE,
+	INDEX `darkchat_messages_darkchat_channels_id_fk` (`channel_id`) USING BTREE,
+	CONSTRAINT `darkchat_messages_darkchat_channels_id_fk` FOREIGN KEY (`channel_id`) REFERENCES `npwd_darkchat_channels` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=31
+;
+
+ALTER TABLE players ADD COLUMN `phone_number` VARCHAR(20) DEFAULT NULL;
 
 ALTER TABLE `mdw_announcements`
   ADD PRIMARY KEY (`id`);
